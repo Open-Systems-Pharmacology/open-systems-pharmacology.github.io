25d0689fc77835976f5307a38be9b9776e5168c4
Michael Sevestre <michael@design2code.ca>
2019-07-04 14:28:31 -0400

    Fixes #54 update to use aos

--- js/site.js
+++ js/site.js
@@ -1,9 +1,13 @@
 $(document).ready(function () {
-  wow = new WOW({
-    animateClass: "animated",
-    offset: 100
+  AOS.init({
+    // Global settings:
+    animatedClassName: 'animated', // class applied on animation
+    offset: 100, // offset (in px) from the original trigger point
+    delay: 100, // values from 0 to 3000, with step 50ms
+    duration: 2000, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: true, // whether animation should happen only once - while scrolling down
   });
-  wow.init();
 
   // jQuery for page scrolling feature - requires jQuery Easing plugin
   $(function () {
